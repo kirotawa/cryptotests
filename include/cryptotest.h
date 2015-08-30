39d30cd90dd8b28e1dc543a1a92c8a81cdba6775
Leonidas S Barbosa <kirotawa@gmail.com>
2015-08-29 22:58:52 -0300

    little update

--- include/cryptotest.h
+++ include/cryptotest.h
@@ -1,14 +1,14 @@
 #ifndef __CRYPTOTEST
 #define __CRYPTOTEST
 
-#define AES 	0x0
-#define CBC 	0x1
-#define CTR 	0x2
-#define ECB 	0x3
-#define GCM 	0x4
-#define GHASH 	0x5
-#define SHA256  0x6
-#define SHA512	0x7
+#define AES 	0x00
+#define CBC 	0x01
+#define CTR 	0x02
+#define ECB 	0x03
+#define GCM 	0x04
+#define GHASH 	0x05
+#define SHA256  0x06
+#define SHA512	0x07
 
 #define hexdump(value) { \
 	print_hex_dump(KERN_CONT, "", DUMP_PREFIX_OFFSET, \
